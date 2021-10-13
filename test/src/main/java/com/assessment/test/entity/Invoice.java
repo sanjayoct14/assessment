5d95dfb9d9597fd1c09e0959afbe4e33d184b4c6
sanjayoct14 <chaurasia.sanju91@gmail.com>
2021-10-13 13:44:45 +0530

    added space

--- test/src/main/java/com/assessment/test/entity/Invoice.java
+++ test/src/main/java/com/assessment/test/entity/Invoice.java
@@ -12,7 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.SequenceGenerator; 
 
 @Entity
 public class Invoice {
